e7e33a3ac4251e6d9afad3cd21b8dc5632a955a7
Malina Ierome <mierome@tunego.com>
2020-02-07 15:51:39 -0800

    Initial Commit

---
+++ SwiftCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by Malina Ierome on 2/7/20.
+//  Copyright © 2020 Malina Ierome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
